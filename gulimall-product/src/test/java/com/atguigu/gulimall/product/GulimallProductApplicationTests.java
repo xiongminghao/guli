9434eec23f63d789c9131b8c49cf65c06f9fd3cf
xiongminghao <332382229@qq.com>
2022-07-02 16:27:58 +0800

    完成开发环境配置

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,22 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import lombok.val;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
-
+	@Autowired
+	BrandService brandService;
 	@Test
 	void contextLoads() {
+		BrandEntity brandEntity = brandService.getById(31);
+		brandEntity.setName("胡凯莉");
+		brandService.updateById(brandEntity);
+		System.out.println("修改成功");
 	}
 
 }
